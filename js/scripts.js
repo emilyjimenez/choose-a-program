c918c57116daafa8cdf224132f408d21d8514af5
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-14 11:51:48 -0700

    add initial form set up.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    event.preventDefault();
 
+
+
+    $("#results").removeClass("hide");
+    $("#results p").text(pokemon);
+    $ ("#results img").attr("src", )
+  });
+});
