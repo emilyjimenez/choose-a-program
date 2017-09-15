bd457ed39cc2fc778361a047e1b7992f5ba86a89
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-15 16:51:16 -0700

    define variables, create if statement for if you are lazy.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
+    var outcome;
+    var pokemonimg;
+    var name = $("input#name").val();
+    var personality = $("input:radio[name=personality]checked").val();
+    var celebrity = $("#celebrity")
+    var development = $("input:radio[name=development]:checked").val();
+    var ready = $("input:radio[name=ready]checked").val();
 
-
+    if (personality === "lazy") {
+      outcome = "Coding is not for you";
+    }
 
     $("#results").removeClass("hide");
-    $("#results p").text();
-    $ ("#results img").attr("src", )
+    $("#results p").text(outcome);
+    // $ ("#results img").attr("src", );
   });
 });
