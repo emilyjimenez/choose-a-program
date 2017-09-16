f1a0c050e7d1843cef9f830fb3266590abae4747
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-15 18:03:13 -0700

    edit .text(outcome) so it shows after form is submitted.

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,14 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var outcome;
-    var pokemonimg;
     var name = $("input#name").val();
-    var personality = $("input:radio[name=personality]checked").val();
-    var celebrity = $("#celebrity")
+    var personalityTrait = $("input:radio[name=personality]checked").val();
+    var celebrity = $("#celebrity");
     var development = $("input:radio[name=development]:checked").val();
     var ready = $("input:radio[name=ready]checked").val();
 
-    if (personality === "lazy") {
-      outcome = "Coding is not for you";
+    if (personalityTrait === "lazy") {
+      outcome = "coding is not for you"
     }
 
     $("#results").removeClass("hide");
