a98de0407650b6d1b4df4780bad20b222a556b89
Emily Wells Jimenez <emwellspdx@gmail.com>
2017-09-17 15:25:43 -0700

    add branch for if you arent ready to study, coding is not for you, and if you are unsure about back end vs front end, results will show choosing java/android.

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,15 @@ $(document).ready(function() {
     var personality = $("input:radio[name=personality]:checked").val();
     var celebrity = $("#celebrity");
     var development = $("input:radio[name=development]:checked").val();
-    var ready = $("input:radio[name=ready]checked").val();
+    var ready = $("input:radio[name=ready]:checked").val();
 
-    if (personality === "lazy"){
+    if (personality === "lazy" || ready === "no"){
       result = "Coding is not for you.";
       codeimg = "img/defeated.gif";
-    }
+    } else if (development === "idk") {
+      result = "Java/Android! Maybe you'd like to make a game or a helpful app since you arent sure about back-end or front-end development."
+      codeimg = "img/android.gif"
+    } else if
 
     $("#results").show();
     $("#results p").text(result);
